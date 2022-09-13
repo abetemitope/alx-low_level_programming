d4813f37b15ad58f448b98472b355f459b6db1f7
Abe Temitope <abetemitope@gmail.com>
2022-09-13 10:57:53 -0700

    teting

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
